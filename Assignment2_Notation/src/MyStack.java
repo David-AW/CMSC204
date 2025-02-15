bdbcc424c58754789aa1e4b2da6ea17d2222106d
david-aw <30061695+David-AW@users.noreply.github.com>
2025-02-15 14:23:29 -0500

    MyStack class setup

---
+++ Assignment2_Notation/src/MyStack.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+
+public class MyStack<T> implements StackInterface<T>{
+
+	Object[] storage;
+	
+	public MyStack() {
+		this(16);
+	}
+	
+	public MyStack(int stack_size) {
+		storage = new Object[stack_size];
+	}
+	
+	@Override
+	public boolean push(T e) throws StackOverflowException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	@Override
+	public T pop() throws StackUnderflowException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public void fill(ArrayList<T> list) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isFull() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public T top() throws StackUnderflowException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String toString(String delimiter) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
