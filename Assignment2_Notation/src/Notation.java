4d3454a372f22bb887a7db7ec0ea9813f2a26ab1
david-aw <30061695+David-AW@users.noreply.github.com>
2025-02-16 12:35:16 -0500

    Create Notation class and corresponding InvalidNotationFormatException

---
+++ Assignment2_Notation/src/Notation.java
@@ -0,0 +1,37 @@
+
+public class Notation {
+
+	/**
+	 * Convert the Postfix expression to the Infix expression
+	 * @param postfix the postfix expression in string format
+	 * @return the infix expression in string format
+	 * @throws InvalidNotationFormatException if the postfix expression format is invalid
+	 */
+	public static String convertPostfixToInfix(String postfix) throws InvalidNotationFormatException{
+	
+		return null;
+	}
+	
+	/**
+	 * Convert an infix expression into a postfix expression
+	 * @param infix the infix expression in string format
+	 * @return the postfix expression in string format
+	 * @throws InvalidNotationFormatException if the infix expression format is invalid
+	 */
+	public static String convertInfixToPostfix(String infix) throws InvalidNotationFormatException {
+		
+		return null;
+	}
+	
+	/**
+	 * Evaluates a postfix expression from a string to a double
+	 * @param postfix the postfix expression in String format
+	 * @return the evaluation of the postfix expression as a double
+	 * @throws InvalidNotationFormatException if the postfix expression format is invalid
+	 */
+	public static double evaluatePostfixExpression​(String postfix) throws InvalidNotationFormatException {
+		
+		return 0;
+	}
+	
+}
