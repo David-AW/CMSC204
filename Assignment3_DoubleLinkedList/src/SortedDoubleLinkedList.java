243d167f5eb0d364de8c41a9206744d750f6bc3e
david-aw <30061695+David-AW@users.noreply.github.com>
2025-03-11 22:38:02 -0400

    Implement sorted functionality and remove optimization

--- Assignment3_DoubleLinkedList/src/SortedDoubleLinkedList.java
+++ Assignment3_DoubleLinkedList/src/SortedDoubleLinkedList.java
@@ -2,12 +2,64 @@ import java.util.Comparator;
 
 public class SortedDoubleLinkedList<T> extends BasicDoubleLinkedList<T>{
 
+	Comparator<T> comparator;
+	
 	public SortedDoubleLinkedList(Comparator<T> comparator) {
-		
+		this.comparator = comparator;
 	}
 
 	public void add(T data) {
+		Node temp = new Node(data);
+		size++;
+		if (head == null) {
+			head = temp;
+			tail = head;
+			return;
+		}
 		
+		Node current_node = head;
+		while(current_node != null) {
+			if (comparator.compare(current_node.data, data) >= 0) {
+				if (current_node.prev == null) { // This is true only if the item is less than the head node, so we set this item to the new head
+					head = temp;
+					temp.next = current_node;
+					current_node.prev = temp;
+				}else { // Splice this item into the double linked list at the position BEFORE the current node which is larger in comparison
+					temp.prev = current_node.prev;
+					current_node.prev.next = temp;
+					current_node.prev = temp;
+					temp.next = current_node;
+				}
+				return; // We found a spot for this item so exit this method
+			}
+			current_node = current_node.next;
+		}
+		
+		// If we reached the end of the linked list, then go to the tail node and add item as the new tail
+		if (current_node == null)
+			current_node = tail;
+		current_node.next = temp;
+		temp.prev = current_node;
+		tail = temp;
+	}
+	
+	@Override
+	public Node remove(T data, Comparator<T> comparator) {
+		if (head == null)
+			return null;
+		Node current_node = head;
+		int comparison;
+		while (current_node != null) {
+			comparison = comparator.compare(current_node.data, data);
+			if (comparison == 0) {
+				remove(current_node);
+				break;
+			}else if (comparison > 0) {
+				return null; // return null because the item does not exist if there is only larger items left in a sorted list
+			}
+			current_node = current_node.next;
+		}
+		return current_node;
 	}
 	
 	/**
