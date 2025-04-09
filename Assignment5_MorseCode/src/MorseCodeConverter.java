fb36a3dc540f515f31fa7dec145fe1775e588d77
david-aw <30061695+David-AW@users.noreply.github.com>
2025-04-09 13:08:08 -0400

    Implements translating a string of morse to english

--- Assignment5_MorseCode/src/MorseCodeConverter.java
+++ Assignment5_MorseCode/src/MorseCodeConverter.java
@@ -5,9 +5,15 @@ public class MorseCodeConverter {
 
 	private static MorseCodeTree tree = new MorseCodeTree();
 	
-	public static String convertToEnglish(String string) {
-		
-		return null;
+	public static String convertToEnglish(String sentence) {
+		String decoded = "";
+		for (String word : sentence.split("/")) {
+			for (String letter : word.split(" ")) {
+				decoded += tree.fetch(letter);
+			}
+			decoded += " ";
+		}
+		return decoded.strip();
 	}
 
 	public static String convertToEnglish(File file) throws FileNotFoundException{
