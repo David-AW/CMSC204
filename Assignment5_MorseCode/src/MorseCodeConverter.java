81584053cf0ad8422fa037e74330404c141d92c6
david-aw <30061695+David-AW@users.noreply.github.com>
2025-04-19 21:44:51 -0400

    Add JavaDoc comments

--- Assignment5_MorseCode/src/MorseCodeConverter.java
+++ Assignment5_MorseCode/src/MorseCodeConverter.java
@@ -8,6 +8,11 @@ public class MorseCodeConverter {
 
 	private static MorseCodeTree tree = new MorseCodeTree();
 	
+	/**
+	 * Converts a morse code sentence where letters are separated by the "space" character and words are separated by a slash "/".
+	 * @param sentence Sentence to convert
+	 * @return English conversion of the morse code
+	 */
 	public static String convertToEnglish(String sentence) {
 		String decoded = "";
 		for (String word : sentence.split("/")) {
@@ -19,6 +24,12 @@ public class MorseCodeConverter {
 		return decoded.strip();
 	}
 
+	/**
+	 * Converts a morse code sentence where letters are separated by the "space" character and words are separated by a slash "/".
+	 * @param file File containing morse code to convert
+	 * @return English conversion of the morse code
+	 * @throws FileNotFoundException If the file specified doesn't exist
+	 */
 	public static String convertToEnglish(File file) throws FileNotFoundException{
 		BufferedReader reader = new BufferedReader(new FileReader(file));
 		String morse = "";
@@ -33,6 +44,10 @@ public class MorseCodeConverter {
 		return convertToEnglish(morse.strip());
 	}
 
+	/**
+	 * Prints the tree in LNR (post)order separated by spaces
+	 * @return the postorder representation of the tree
+	 */
 	public static String printTree() {
 		String output = "";
 		for (String c : tree.toArrayList())
