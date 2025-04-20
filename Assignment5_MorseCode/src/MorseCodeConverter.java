4492b0db825cc2487227f18932104a411165080a
david-aw <30061695+David-AW@users.noreply.github.com>
2025-04-19 21:35:31 -0400

    Implement converting morse file to english

--- Assignment5_MorseCode/src/MorseCodeConverter.java
+++ Assignment5_MorseCode/src/MorseCodeConverter.java
@@ -1,5 +1,8 @@
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class MorseCodeConverter {
 
@@ -17,8 +20,17 @@ public class MorseCodeConverter {
 	}
 
 	public static String convertToEnglish(File file) throws FileNotFoundException{
-		
-		return null;
+		BufferedReader reader = new BufferedReader(new FileReader(file));
+		String morse = "";
+		try {
+			while (reader.ready()) {
+				morse += reader.readLine().strip() + " ";
+			}
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return convertToEnglish(morse.strip());
 	}
 
 	public static String printTree() {
